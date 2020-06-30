c9fc18c1b7e8f7b555ce16a5ef76b3c43db7e3eb
Jan <jan.andrade@capgemini.com>
2020-06-30 00:45:09 -0300

    almost done

--- App.js
+++ App.js
@@ -8,13 +8,22 @@ import {
 	FlatList,
 	Dimensions,
 	Image,
+	Animated,
+	TouchableWithoutFeedback,
+	TouchableOpacity,
+	/* 	CameraRoll, */
+	Share,
 } from 'react-native';
 import axios from 'axios';
+import { Ionicons } from '@expo/vector-icons';
 import { ACCESS_KEY } from 'react-native-dotenv';
+/* import { Permissions, FileSystem } from 'expo'; */
 
 export default function App() {
 	const [isLoading, setIsLoading] = useState(true);
 	const [images, setImages] = useState([]);
+	const [scale, setScale] = useState('0px');
+	const [isImageFocused, setIsImageFocused] = useState(false);
 	const { height, width } = Dimensions.get('window');
 
 	useEffect(() => {
@@ -35,18 +44,171 @@ export default function App() {
 			});
 	}
 
+	/* async function saveToCameraRoll(item) {
+		let cameraPermissions = await Permissions.getAsync(
+			Permissions.CAMERA_ROLL
+		);
+
+		if (cameraPermissions.status !== 'granted') {
+			cameraPermissions = await Permissions.askAsync(
+				Permissions.CAMERA_ROLL
+			);
+		}
+
+		if (cameraPermissions.status === 'granted') {
+			FileSystem.downloadAsync(
+				image.urls.regular,
+				FileSystem.documentDirectory + image.id + '.jpg'
+			)
+				.then(({ uri }) => {
+					CameraRoll.saveToCameraRoll(uri);
+					alert('Saved to galery');
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		} else {
+			alert('Requires camera roll permissions');
+		}
+	} */
+
+	function showControls(item) {
+		setIsImageFocused(!isImageFocused);
+		if (isImageFocused === true) {
+			setScale('0px');
+		} else {
+			setScale('30px');
+		}
+	}
+
+	async function shareWallPaper(image) {
+		try {
+			await Share.share({
+				message: 'Checkout this wallpaper ' + image.urls.full,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
 	function renderItem(image) {
 		return (
-			<View style={{ height, width }}>
-				<Image
+			<View style={{ flex: 1 }}>
+				<View
 					style={{
-						flex: 1,
-						height: null,
-						width: null,
+						position: 'absolute',
+						top: 0,
+						right: 0,
+						left: 0,
+						bottom: 0,
+						backgroundColor: 'black',
+						alignItems: 'center',
+						justifyContent: 'center',
 					}}
-					source={{ uri: image.urls.regular }}
-					resizeMode="cover"
-				/>
+				>
+					<ActivityIndicator size="large" color="grey" />
+				</View>
+				<TouchableWithoutFeedback
+					onPress={(item) => showControls(item)}
+				>
+					<Animated.View
+						style={{
+							height,
+							width,
+							padding: scale,
+						}}
+					>
+						<Image
+							style={{
+								flex: 1,
+								height: null,
+								width: null,
+							}}
+							source={{ uri: image.urls.regular }}
+							resizeMode="cover"
+						/>
+					</Animated.View>
+				</TouchableWithoutFeedback>
+
+				{isImageFocused ? (
+					<Animated.View
+						style={{
+							position: 'absolute',
+							left: 0,
+							right: 0,
+							bottom: 0,
+							height: 80,
+							backgroundColor: 'black',
+							flexDirection: 'row',
+							justifyContent: 'space-around',
+						}}
+					>
+						<View
+							style={{
+								flex: 1,
+								alignItems: 'center',
+								justifyContent: 'center',
+							}}
+						>
+							<TouchableOpacity
+								activeOpacity={0.5}
+								onPress={() => loadWallPapers()}
+							>
+								<Ionicons
+									name="ios-refresh"
+									color="white"
+									size={40}
+								/>
+							</TouchableOpacity>
+						</View>
+
+						<View
+							style={{
+								flex: 1,
+								alignItems: 'center',
+								justifyContent: 'center',
+							}}
+						>
+							<TouchableOpacity
+								activeOpacity={0.5}
+								onPress={(item) =>
+									/* saveToCameraRoll(item) */ alert(
+										'Unable to save yet'
+									)
+								}
+							>
+								<Ionicons
+									name="ios-share"
+									color="white"
+									size={40}
+								/>
+							</TouchableOpacity>
+						</View>
+
+						<View
+							style={{
+								flex: 1,
+								alignItems: 'center',
+								justifyContent: 'center',
+							}}
+						>
+							<TouchableOpacity
+								activeOpacity={0.5}
+								onPress={(item) =>
+									shareWallPaper(item)
+								}
+							>
+								<Ionicons
+									name="ios-save"
+									color="white"
+									size={40}
+								/>
+							</TouchableOpacity>
+						</View>
+					</Animated.View>
+				) : (
+					<></>
+				)}
 			</View>
 		);
 	}
@@ -77,6 +239,7 @@ export default function App() {
 					<FlatList
 						horizontal
 						pagingEnabled
+						scrollEnabled={!isImageFocused}
 						data={images}
 						renderItem={({ item }) => renderItem(item)}
 						keyExtractor={(item) => item.id}
